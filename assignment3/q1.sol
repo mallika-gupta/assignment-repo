03f0376bfe2aecd4914c6085f05c091fecb8422b
mallika-gupta <93383830+mallika-gupta@users.noreply.github.com>
2024-08-23 22:22:32 +0530

    Update q1.sol

--- assignment3/q1.sol
+++ assignment3/q1.sol
@@ -1,1 +1,38 @@
-ab
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address public manager;
+    address payable[] public players;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether, "Minimum ether not sent");
+        players.push(payable(msg.sender));
+    }
+
+   function random() private view returns (uint) {
+    return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players)));
+}
+
+   function pickWinner() public restricted {
+        uint index = random() % players.length;
+        address payable winner = players[index];
+        winner.transfer(address(this).balance);
+        
+        // Clear the array
+        delete players;  
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, "Only manager can call this function");
+        _;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+}
